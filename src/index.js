bb8895d4c3398df6d54a3033ed5a9f33073dbf44
Viktor <setrac91@gmail.com>
2023-10-09 20:42:15 +0400

    feat: add logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stackOfStr =[];
+
+  const Open_Brackets = [];
+  const Close_Brackets = [];
+  for (let a = 0; a < bracketsConfig.lenght; a=a+2){
+    let oBrackets = bracketsConfig[a];
+      Open_Brackets.push(oBrackets);
+  }
+
+  for (let a = 0; a < bracketsConfig.lenght; a=a+2){
+    for (let b = 1; b < bracketsConfig.lenght; b=b+2){
+    //for (let a = 0; a < bracketsConfig.lenght; a=a+2){
+    let cBrackets = bracketsConfig[b];
+      Close_Brackets.push(bracketsConfig[a]+':'+cBrackets);
+      console.log(Close_Brackets);
+  }
+}
+
+  for (let i=0; i< str.lenght; i++){
+    let currentSymbol = str[i];
+
+    if(Open_Brackets.includes(currentSymbol)){
+      stackOfStr.push(currentSymbol);
+    } else {
+      if (stackOfStr.length === 0) {
+        return false;
+      }
+      let topElement = stackOfStr[stackOfStr.length -1];
+
+      if(Close_Brackets[currentSymbol] === topElement){
+        stackOfStr.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stackOfStr.length === 0;
 }
